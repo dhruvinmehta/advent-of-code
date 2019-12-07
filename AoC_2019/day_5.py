be9c00213faab9d32313b0c66043b7db41ac478a
Dhruvin Mehta <dhruvin9311@gmail.com>
2019-12-07 17:01:12 +0530

    Modified day 5 solution for day 7 challenge

--- AoC_2019/day_5.py
+++ AoC_2019/day_5.py
@@ -7,99 +7,108 @@ def read_input_file(file):
         return [int(element) for element in file.read().split(",")]
 
 
-def value(instructions, index, mode):
-    if mode == 0:
-        return instructions[instructions[index]]
-    else:
-        return instructions[index]
+class Computer:
+    def __init__(self, instructions):
+        self.instructions = instructions[:]
+        self.pointer = 0
+        self.output = 0
 
+    def value(self, index, mode):
+        if mode == 0:
+            return self.instructions[self.instructions[index]]
+        else:
+            return self.instructions[index]
 
-def decode(opcode):
-    opcode_str = str(opcode).zfill(4)
-    return int(opcode_str[1]), int(opcode_str[0]), int(opcode_str[-2:])
+    @staticmethod
+    def decode(opcode):
+        opcode_str = str(opcode).zfill(4)
+        return int(opcode_str[1]), int(opcode_str[0]), int(opcode_str[-2:])
 
+    def first_pos(self, kwargs):
+        return self.value(self.pointer + 1, kwargs.get('mode1'))
 
-def opcode1(instructions, index, **kwargs):
-    instructions[instructions[index + 3]] = value(instructions, index + 1, kwargs.get('mode1')) + value(instructions, index + 2, kwargs.get('mode2'))
-    return index + 4
+    def second_pos(self, kwargs):
+        return self.value(self.pointer + 2, kwargs.get('mode2'))
 
+    def opcode1(self, **kwargs):
+        self.instructions[self.instructions[self.pointer + 3]] = self.first_pos(kwargs) + self.second_pos(kwargs)
+        self.pointer += 4
 
-def opcode2(instructions, index, **kwargs):
-    instructions[instructions[index + 3]] = value(instructions, index + 1, kwargs.get('mode1')) * value(instructions, index + 2, kwargs.get('mode2'))
-    return index + 4
+    def opcode2(self, **kwargs):
+        self.instructions[self.instructions[self.pointer + 3]] = self.first_pos(kwargs) * self.second_pos(kwargs)
+        self.pointer += 4
 
+    def opcode3(self, **kwargs):
+        self.instructions[self.instructions[self.pointer + 1]] = kwargs.get('input_val')
+        self.pointer += 2
 
-def opcode3(instructions, index, **kwargs):
-    instructions[instructions[index + 1]] = kwargs.get('input_val')
-    return index + 2
+    def opcode4(self, **kwargs):
+        self.output = self.first_pos(kwargs)
+        self.pointer += 2
 
+    def opcode5(self, **kwargs):
+        if self.first_pos(kwargs) != 0:
+            self.pointer = self.second_pos(kwargs)
+        else:
+            self.pointer += 3
 
-def opcode4(instructions, index, **kwargs):
-    kwargs.get('outputs').append(value(instructions, index + 1, kwargs.get('mode1')))
-    return index + 2
+    def opcode6(self, **kwargs):
+        if self.first_pos(kwargs) == 0:
+            self.pointer = self.second_pos(kwargs)
+        else:
+            self.pointer += 3
 
+    def opcode7(self, **kwargs):
+        if self.first_pos(kwargs) < self.second_pos(kwargs):
+            self.instructions[self.instructions[self.pointer + 3]] = 1
+        else:
+            self.instructions[self.instructions[self.pointer + 3]] = 0
+        self.pointer += 4
 
-def opcode5(instructions, index, **kwargs):
-    if value(instructions, index + 1, kwargs.get('mode1')) != 0:
-        return value(instructions, index + 2, kwargs.get('mode2'))
-    else:
-        return index + 3
+    def opcode8(self, **kwargs):
+        if self.first_pos(kwargs) == self.second_pos(kwargs):
+            self.instructions[self.instructions[self.pointer + 3]] = 1
+        else:
+            self.instructions[self.instructions[self.pointer + 3]] = 0
+        self.pointer += 4
 
+    OPCODE_MAP = {1: opcode1, 2: opcode2, 3: opcode3, 4: opcode4, 5: opcode5, 6: opcode6, 7: opcode7, 8: opcode8}
 
-def opcode6(instructions, index, **kwargs):
-    if value(instructions, index + 1, kwargs.get('mode1')) == 0:
-        return value(instructions, index + 2, kwargs.get('mode2'))
-    else:
-        return index + 3
+    def execute(self, input_val=1, is_amplifier=False, in_phase=False, phase_val=0, feedback=False):
+        while self.instructions[self.pointer] != 99:
+            mode1, mode2, opcode = self.decode(self.instructions[self.pointer])
 
+            temp_val = input_val if in_phase else phase_val if is_amplifier else input_val
+            Computer.OPCODE_MAP[opcode](self, mode1=mode1, mode2=mode2, input_val=temp_val)
 
-def opcode7(instructions, index, **kwargs):
-    if value(instructions, index + 1, kwargs.get('mode1')) < value(instructions, index + 2, kwargs.get('mode2')):
-        instructions[instructions[index + 3]] = 1
-    else:
-        instructions[instructions[index + 3]] = 0
-    return index + 4
+            if opcode == 3 and is_amplifier:
+                in_phase = True
 
+            if opcode == 4 and feedback:
+                return self.output, False
 
-def opcode8(instructions, index, **kwargs):
-    if value(instructions, index + 1, kwargs.get('mode1')) == value(instructions, index + 2, kwargs.get('mode2')):
-        instructions[instructions[index + 3]] = 1
-    else:
-        instructions[instructions[index + 3]] = 0
-    return index + 4
-
-
-OPCODE_MAP = {1: opcode1, 2: opcode2, 3: opcode3, 4: opcode4, 5: opcode5, 6: opcode6, 7: opcode7, 8: opcode8}
-
-
-def execute(instructions, input_val=1):
-    outputs = []
-    index = 0
-    while instructions[index] != 99:
-        mode1, mode2, opcode = decode(instructions[index])
-        index = OPCODE_MAP[opcode](instructions, index, mode1=mode1, mode2=mode2, input_val=input_val, outputs=outputs)
-
-    return outputs
+        return self.output, True
 
 
 # Unit tests
-assert execute([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], 8) == [1]
-assert execute([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], 3) == [0]
-assert execute([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], 5) == [1]
-assert execute([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], 8) == [0]
-assert execute([3, 3, 1108, -1, 8, 3, 4, 3, 99], 8) == [1]
-assert execute([3, 3, 1108, -1, 8, 3, 4, 3, 99], 10) == [0]
-assert execute([3, 3, 1107, -1, 8, 3, 4, 3, 99], 6) == [1]
-assert execute([3, 3, 1107, -1, 8, 3, 4, 3, 99], 9) == [0]
-assert execute([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], 0) == [0]
-assert execute([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], 5) == [1]
-assert execute([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1], 0) == [0]
-assert execute([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1], 8) == [1]
+assert Computer([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]).execute(8) == (1, True)
+assert Computer([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]).execute(3) == (0, True)
+assert Computer([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]).execute(5) == (1, True)
+assert Computer([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]).execute(8) == (0, True)
+assert Computer([3, 3, 1108, -1, 8, 3, 4, 3, 99]).execute(8) == (1, True)
+assert Computer([3, 3, 1108, -1, 8, 3, 4, 3, 99]).execute(10) == (0, True)
+assert Computer([3, 3, 1107, -1, 8, 3, 4, 3, 99]).execute(6) == (1, True)
+assert Computer([3, 3, 1107, -1, 8, 3, 4, 3, 99]).execute(9) == (0, True)
+assert Computer([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9]).execute(0) == (0, True)
+assert Computer([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9]).execute(5) == (1, True)
+assert Computer([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1]).execute(0) == (0, True)
+assert Computer([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1]).execute(8) == (1, True)
 
 
 def solution():
-    print("Part 1 ans", execute(read_input_file("Files/day-5-input.txt"))[-1])
-    print("Part 2 ans", execute(read_input_file("Files/day-5-input.txt"), 5)[-1])
+    print("Part 1 ans", Computer(read_input_file("Files/day-5-input.txt")).execute()[0])
+    print("Part 2 ans", Computer(read_input_file("Files/day-5-input.txt")).execute(5)[0])
 
 
-solution()
+# Uncomment below line to execute the code
+# solution()
